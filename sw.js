46d49e5c1e80454ccec0bede02dc699aa08fa653
Johannes Leipold <92841675+Jo1931@users.noreply.github.com>
2026-09-26 00:56:36 +0200

    Refresh cache for Skull King setup change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spielzaehler-v15";
+const CACHE_NAME = "spielzaehler-v16";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./nochmal.js", "./skullking.js", "./manifest.webmanifest", "./icons/icon.svg", "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
