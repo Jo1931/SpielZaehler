859d967b568dd1e76ed5742473a2c420b3c8f433
Johannes Leipold <92841675+Jo1931@users.noreply.github.com>
2026-09-26 00:59:27 +0200

    Refresh cache for Skull King archive fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spielzaehler-v17";
+const CACHE_NAME = "spielzaehler-v18";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./nochmal.js", "./skullking.js", "./manifest.webmanifest", "./icons/icon.svg", "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
