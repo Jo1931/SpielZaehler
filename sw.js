e58db7cab468252d7ac41e8fd9f12644002c9faf
Johannes Leipold <92841675+Jo1931@users.noreply.github.com>
2026-09-26 10:42:31 +0200

    Refresh cache for passive player board selection fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spielzaehler-v45";
+const CACHE_NAME = "spielzaehler-v46";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./nochmal.js", "./skullking.js", "./manifest.webmanifest", "./icons/icon.svg", "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
