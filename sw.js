2da48781d4fa671996f53544a41bdfd7bb39a92f
Johannes Leipold <92841675+Jo1931@users.noreply.github.com>
2026-09-26 10:33:59 +0200

    Refresh cache for active dice selection fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spielzaehler-v42";
+const CACHE_NAME = "spielzaehler-v43";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./nochmal.js", "./skullking.js", "./manifest.webmanifest", "./icons/icon.svg", "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
